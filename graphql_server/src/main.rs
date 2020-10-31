30334846fe014dce4597acbcd87a55d8e2ab9cda
certainty <david.krentzlin@gmail.com>
2020-10-31 08:11:16 +0100

    Extract schema into its own file

--- graphql_server/src/main.rs
+++ graphql_server/src/main.rs
@@ -7,105 +7,7 @@ use juniper::http::GraphQLRequest;
 use std::io;
 use std::sync::Arc;
 
-use juniper::FieldResult;
-
-struct DatabasePool;
-impl DatabasePool {
-    fn get_connection(&self) -> FieldResult<DatabasePool> {
-        Ok(DatabasePool)
-    }
-    fn find_human(&self, _id: &str) -> FieldResult<Human> {
-        Err("")?
-    }
-    fn insert_human(&self, _human: &NewHuman) -> FieldResult<Human> {
-        Err("")?
-    }
-}
-
-#[derive(juniper::GraphQLEnum)]
-enum Episode {
-    NewHope,
-    Empire,
-    Jedi,
-}
-
-#[derive(juniper::GraphQLObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct Human {
-    id: String,
-    name: String,
-    appears_in: Vec<Episode>,
-    home_planet: String,
-}
-
-// There is also a custom derive for mapping GraphQL input objects.
-
-#[derive(juniper::GraphQLInputObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct NewHuman {
-    name: String,
-    appears_in: Vec<Episode>,
-    home_planet: String,
-}
-
-// Now, we create our root Query and Mutation types with resolvers by using the
-// object macro.
-// Objects can have contexts that allow accessing shared state like a database
-// pool.
-
-struct Context {
-    // Use your real database pool here.
-    pool: DatabasePool,
-}
-
-// To make our context usable by Juniper, we have to implement a marker trait.
-impl juniper::Context for Context {}
-
-struct Query;
-
-#[juniper::object(
-    // Here we specify the context type for the object.
-    // We need to do this in every type that
-    // needs access to the context.
-    Context = Context,
-)]
-impl Query {
-    fn apiVersion() -> &str {
-        "1.0"
-    }
-
-    // Arguments to resolvers can either be simple types or input objects.
-    // To gain access to the context, we specify a argument
-    // that is a reference to the Context type.
-    // Juniper automatically injects the correct context here.
-    fn human(context: &Context, id: String) -> FieldResult<Human> {
-        // Get a db connection.
-        let connection = context.pool.get_connection()?;
-        // Execute a db query.
-        // Note the use of `?` to propagate errors.
-        let human = connection.find_human(&id)?;
-        // Return the result.
-        Ok(human)
-    }
-}
-
-// Now, we do the same for our Mutation type.
-struct Mutation;
-
-#[juniper::object(
-    Context = Context,
-)]
-impl Mutation {
-    fn createHuman(context: &Context, new_human: NewHuman) -> FieldResult<Human> {
-        let db = executor.context().pool.get_connection()?;
-        let human: Human = db.insert_human(&new_human)?;
-        Ok(human)
-    }
-}
-
-// A root schema consists of a query and a mutation.
-// Request queries can be executed against a RootNode.
-type Schema = juniper::RootNode<'static, Query, Mutation>;
+pub mod schema;
 
 async fn graphiql() -> HttpResponse {
     let html = graphiql_source("http://127.0.0.1:8080/graphql");
@@ -115,16 +17,11 @@ async fn graphiql() -> HttpResponse {
 }
 
 async fn graphql(
-    st: web::Data<Arc<Schema>>,
+    st: web::Data<Arc<schema::Schema>>,
     data: web::Json<GraphQLRequest>,
 ) -> Result<HttpResponse, Error> {
     let user = web::block(move || {
-        let res = data.execute(
-            &st,
-            &Context {
-                pool: DatabasePool {},
-            },
-        );
+        let res = data.execute(&st, &schema::Context::new());
         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
     })
     .await?;
@@ -139,7 +36,7 @@ async fn main() -> io::Result<()> {
     env_logger::init();
 
     // Create Juniper schema
-    let schema = std::sync::Arc::new(Schema::new(Query, Mutation));
+    let schema = std::sync::Arc::new(schema::build());
     println!("Access service at http://localhost:8080/graphiql");
 
     // Start http server
--- graphql_server/src/schema.rs
+++ graphql_server/src/schema.rs
@@ -1,382 +1,112 @@
-use std::{collections::HashMap, pin::Pin};
+use juniper::FieldResult;
 
-use juniper::{
-    graphql_interface, graphql_object, CharacterValue, Context, DefaultScalarValue, GraphQLEnum,
-};
+pub struct DatabasePool;
 
-pub struct Query;
-
-#[graphql_object(context = Database, scalar = DefaultScalarValue)]
-/// The root query object of the schema
-impl Query {
-    #[graphql(arguments(id(description = "id of the human")))]
-    fn human(database: &Database, id: String) -> Option<&Human> {
-        database.get_human(&id)
+impl DatabasePool {
+    fn get_connection(&self) -> FieldResult<DatabasePool> {
+        Ok(DatabasePool)
     }
-
-    #[graphql(arguments(id(description = "id of the droid")))]
-    fn droid(database: &Database, id: String) -> Option<&Droid> {
-        database.get_droid(&id)
+    fn find_human(&self, _id: &str) -> FieldResult<Human> {
+        Err("")?
     }
-
-    #[graphql(arguments(episode(
-        description = "If omitted, returns the hero of the whole saga. \
-                       If provided, returns the hero of that particular episode"
-    )))]
-    fn hero(database: &Database, episode: Option<Episode>) -> Option<CharacterValue> {
-        Some(database.get_hero(episode))
+    fn insert_human(&self, _human: &NewHuman) -> FieldResult<Human> {
+        Err("")?
     }
 }
 
-pub struct Subscription;
+// Now, we create our root Query and Mutation types with resolvers by using the
+// object macro.
+// Objects can have contexts that allow accessing shared state like a database
+// pool.
 
-type HumanStream = Pin<Box<dyn futures::Stream<Item = Human> + Send>>;
+pub struct Context {
+    // Use your real database pool here.
+    pool: DatabasePool,
+}
 
-#[graphql_subscription(context = Database)]
-/// Super basic subscription fixture
-impl Subscription {
-    async fn async_human(context: &Database) -> HumanStream {
-        let human = context.get_human("1000").unwrap().clone();
-        Box::pin(futures::stream::once(futures::future::ready(human)))
+impl Context {
+    pub fn new() -> Context {
+        Context {
+            pool: DatabasePool {},
+        }
     }
 }
-#[derive(GraphQLEnum, Clone, Copy, Debug, Eq, PartialEq)]
-pub enum Episode {
-    #[graphql(name = "NEW_HOPE")]
+
+// To make our context usable by Juniper, we have to implement a marker trait.
+impl juniper::Context for Context {}
+
+#[derive(juniper::GraphQLEnum)]
+enum Episode {
     NewHope,
     Empire,
     Jedi,
 }
 
-#[graphql_interface(for = [Human, Droid], context = Database, scalar = DefaultScalarValue)]
-/// A character in the Star Wars Trilogy
-pub trait Character {
-    /// The id of the character
-    fn id(&self) -> &str;
-
-    /// The name of the character
-    fn name(&self) -> Option<&str>;
-
-    /// The friends of the character
-    fn friends(&self, ctx: &Database) -> Vec<CharacterValue>;
-
-    /// Which movies they appear in
-    fn appears_in(&self) -> &[Episode];
-
-    #[graphql_interface(ignore)]
-    fn friends_ids(&self) -> &[String];
-}
-
-#[derive(Clone)]
-pub struct Human {
+#[derive(juniper::GraphQLObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct Human {
     id: String,
     name: String,
-    friend_ids: Vec<String>,
     appears_in: Vec<Episode>,
-    secret_backstory: Option<String>,
-    home_planet: Option<String>,
+    home_planet: String,
 }
 
-impl Human {
-    pub fn new(
-        id: &str,
-        name: &str,
-        friend_ids: &[&str],
-        appears_in: &[Episode],
-        secret_backstory: Option<&str>,
-        home_planet: Option<&str>,
-    ) -> Self {
-        Self {
-            id: id.to_owned(),
-            name: name.to_owned(),
-            friend_ids: friend_ids
-                .to_owned()
-                .into_iter()
-                .map(ToOwned::to_owned)
-                .collect(),
-            appears_in: appears_in.to_vec(),
-            secret_backstory: secret_backstory.map(ToOwned::to_owned),
-            home_planet: home_planet.map(|p| p.to_owned()),
-        }
-    }
-}
+// There is also a custom derive for mapping GraphQL input objects.
 
-/// A humanoid creature in the Star Wars universe.
-#[graphql_object(
-    context = Database,
-    scalar = DefaultScalarValue,
-    interfaces = CharacterValue,
-)]
-impl Human {
-    /// The id of the human
-    fn id(&self) -> &str {
-        &self.id
-    }
-
-    /// The name of the human
-    fn name(&self) -> Option<&str> {
-        Some(self.name.as_str())
-    }
-
-    /// The friends of the human
-    fn friends(&self, ctx: &Database) -> Vec<CharacterValue> {
-        ctx.get_friends(self)
-    }
-
-    /// Which movies they appear in
-    fn appears_in(&self) -> &[Episode] {
-        &self.appears_in
-    }
-
-    /// The home planet of the human
-    fn home_planet(&self) -> &Option<String> {
-        &self.home_planet
-    }
-}
-
-#[graphql_interface(scalar = DefaultScalarValue)]
-impl Character for Human {
-    fn id(&self) -> &str {
-        &self.id
-    }
-
-    fn name(&self) -> Option<&str> {
-        Some(&self.name)
-    }
-
-    fn friends(&self, ctx: &Database) -> Vec<CharacterValue> {
-        ctx.get_friends(self)
-    }
-
-    fn appears_in(&self) -> &[Episode] {
-        &self.appears_in
-    }
-
-    fn friends_ids(&self) -> &[String] {
-        &self.friend_ids
-    }
-}
-
-#[derive(Clone)]
-pub struct Droid {
-    id: String,
+#[derive(juniper::GraphQLInputObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct NewHuman {
     name: String,
-    friend_ids: Vec<String>,
     appears_in: Vec<Episode>,
-    secret_backstory: Option<String>,
-    primary_function: Option<String>,
+    home_planet: String,
 }
 
-impl Droid {
-    pub fn new(
-        id: &str,
-        name: &str,
-        friend_ids: &[&str],
-        appears_in: &[Episode],
-        secret_backstory: Option<&str>,
-        primary_function: Option<&str>,
-    ) -> Self {
-        Self {
-            id: id.to_owned(),
-            name: name.to_owned(),
-            friend_ids: friend_ids
-                .to_owned()
-                .into_iter()
-                .map(ToOwned::to_owned)
-                .collect(),
-            appears_in: appears_in.to_vec(),
-            secret_backstory: secret_backstory.map(ToOwned::to_owned),
-            primary_function: primary_function.map(ToOwned::to_owned),
-        }
-    }
-}
+pub struct Query;
 
-/// A mechanical creature in the Star Wars universe.
-#[graphql_object(
-    context = Database,
-    scalar = DefaultScalarValue,
-    interfaces = CharacterValue,
+#[juniper::object(
+    // Here we specify the context type for the object.
+    // We need to do this in every type that
+    // needs access to the context.
+    Context = Context,
 )]
-impl Droid {
-    /// The id of the droid
-    fn id(&self) -> &str {
-        &self.id
+impl Query {
+    fn apiVersion() -> &str {
+        "1.0"
     }
 
-    /// The name of the droid
-    fn name(&self) -> Option<&str> {
-        Some(self.name.as_str())
-    }
-
-    /// The friends of the droid
-    fn friends(&self, ctx: &Database) -> Vec<CharacterValue> {
-        ctx.get_friends(self)
-    }
-
-    /// Which movies they appear in
-    fn appears_in(&self) -> &[Episode] {
-        &self.appears_in
-    }
-
-    /// The primary function of the droid
-    fn primary_function(&self) -> &Option<String> {
-        &self.primary_function
+    // Arguments to resolvers can either be simple types or input objects.
+    // To gain access to the context, we specify a argument
+    // that is a reference to the Context type.
+    // Juniper automatically injects the correct context here.
+    fn human(context: &Context, id: String) -> FieldResult<Human> {
+        // Get a db connection.
+        let connection = context.pool.get_connection()?;
+        // Execute a db query.
+        // Note the use of `?` to propagate errors.
+        let human = connection.find_human(&id)?;
+        // Return the result.
+        Ok(human)
     }
 }
 
-#[graphql_interface(scalar = DefaultScalarValue)]
-impl Character for Droid {
-    fn id(&self) -> &str {
-        &self.id
-    }
+// Now, we do the same for our Mutation type.
+pub struct Mutation;
 
-    fn name(&self) -> Option<&str> {
-        Some(&self.name)
-    }
-
-    fn friends(&self, ctx: &Database) -> Vec<CharacterValue> {
-        ctx.get_friends(self)
-    }
-
-    fn appears_in(&self) -> &[Episode] {
-        &self.appears_in
-    }
-
-    fn friends_ids(&self) -> &[String] {
-        &self.friend_ids
+#[juniper::object(
+    Context = Context,
+)]
+impl Mutation {
+    fn createHuman(context: &Context, new_human: NewHuman) -> FieldResult<Human> {
+        let db = executor.context().pool.get_connection()?;
+        let human: Human = db.insert_human(&new_human)?;
+        Ok(human)
     }
 }
 
-#[derive(Default, Clone)]
-pub struct Database {
-    humans: HashMap<String, Human>,
-    droids: HashMap<String, Droid>,
-}
+// A root schema consists of a query and a mutation.
+// Request queries can be executed against a RootNode.
+pub type Schema = juniper::RootNode<'static, Query, Mutation>;
 
-impl Context for Database {}
-
-impl Database {
-    pub fn new() -> Database {
-        let mut humans = HashMap::new();
-        let mut droids = HashMap::new();
-
-        humans.insert(
-            "1000".to_owned(),
-            Human::new(
-                "1000",
-                "Luke Skywalker",
-                &["1002", "1003", "2000", "2001"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                Some("Tatooine"),
-            ),
-        );
-
-        humans.insert(
-            "1001".to_owned(),
-            Human::new(
-                "1001",
-                "Darth Vader",
-                &["1004"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                Some("Tatooine"),
-            ),
-        );
-
-        humans.insert(
-            "1002".to_owned(),
-            Human::new(
-                "1002",
-                "Han Solo",
-                &["1000", "1003", "2001"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                None,
-            ),
-        );
-
-        humans.insert(
-            "1003".to_owned(),
-            Human::new(
-                "1003",
-                "Leia Organa",
-                &["1000", "1002", "2000", "2001"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                Some("Alderaan"),
-            ),
-        );
-
-        humans.insert(
-            "1004".to_owned(),
-            Human::new(
-                "1004",
-                "Wilhuff Tarkin",
-                &["1001"],
-                &[Episode::NewHope],
-                None,
-                None,
-            ),
-        );
-
-        droids.insert(
-            "2000".to_owned(),
-            Droid::new(
-                "2000",
-                "C-3PO",
-                &["1000", "1002", "1003", "2001"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                Some("Protocol"),
-            ),
-        );
-
-        droids.insert(
-            "2001".to_owned(),
-            Droid::new(
-                "2001",
-                "R2-D2",
-                &["1000", "1002", "1003"],
-                &[Episode::NewHope, Episode::Empire, Episode::Jedi],
-                None,
-                Some("Astromech"),
-            ),
-        );
-
-        Database { humans, droids }
-    }
-
-    pub fn get_hero(&self, episode: Option<Episode>) -> CharacterValue {
-        if episode == Some(Episode::Empire) {
-            self.get_human("1000").unwrap().clone().into()
-        } else {
-            self.get_droid("2001").unwrap().clone().into()
-        }
-    }
-
-    pub fn get_human(&self, id: &str) -> Option<&Human> {
-        self.humans.get(id)
-    }
-
-    pub fn get_droid(&self, id: &str) -> Option<&Droid> {
-        self.droids.get(id)
-    }
-
-    pub fn get_character(&self, id: &str) -> Option<CharacterValue> {
-        if let Some(h) = self.humans.get(id) {
-            Some(h.clone().into())
-        } else if let Some(d) = self.droids.get(id) {
-            Some(d.clone().into())
-        } else {
-            None
-        }
-    }
-
-    pub fn get_friends(&self, c: &dyn Character) -> Vec<CharacterValue> {
-        c.friends_ids()
-            .iter()
-            .flat_map(|id| self.get_character(id))
-            .collect()
-    }
+pub fn build() -> Schema {
+    Schema::new(Query, Mutation)
 }
