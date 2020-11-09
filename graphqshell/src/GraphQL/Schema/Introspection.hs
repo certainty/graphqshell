096bdbaf6febb0bc454b37261e2fbeb0009315e7
certainty <david.krentzlin@gmail.com>
2020-11-09 07:52:50 +0100

    Add function to get a schema from introspection
    Parse the introspection response and return the schema

---
+++ graphqshell/src/GraphQL/Schema/Introspection.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE QuasiQuotes, DuplicateRecordFields, DeriveAnyClass #-}
+
+module GraphQL.Schema.Introspection (schemaFromIntrospectionResponse, introspectionQuery, Schema) where
+import Relude hiding (ByteString)
+import Data.Either.Combinators (mapLeft)
+import Text.RawString.QQ
+import Data.ByteString.Lazy
+import Data.Aeson (eitherDecode)
+import GraphQL.Schema.Introspection.Internal
+data IntrospectionError = IntrospectionError String deriving (Eq, Show, Exception)
+
+newtype Schema = Schema {
+  internal :: IntrospectionSchema
+} deriving (Eq, Show)
+
+
+schemaFromIntrospectionResponse ::  ByteString -> Either IntrospectionError Schema
+schemaFromIntrospectionResponse jsonResponse = Schema <$> parseResponse 
+ where
+   parseResponse :: Either IntrospectionError IntrospectionSchema
+   parseResponse = mapLeft IntrospectionError (eitherDecode jsonResponse)
+  
+
+introspectionQuery :: Text
+introspectionQuery = [r|
+query Introspection {
+  schema: __schema {
+    queryType {
+      name 
+    }
+    mutationType {
+      name
+    }
+    subscriptionType {
+      name
+    }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    typeRef: type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+fragment InputValue on __InputValue {
+  name
+  description
+  typeRef: type {
+    ...TypeRef
+  }
+  defaultValue
+}
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}|]
+
+
---
+++ graphqshell/src/GraphQL/Schema/Introspection/Internal.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE QuasiQuotes, DuplicateRecordFields, DeriveAnyClass #-}
+
+module GraphQL.Schema.Introspection.Internal where
+import Relude hiding (ByteString)
+import Data.Aeson.Types (FromJSON)
+
+
+data IntrospectionResponse = IntrospectionResponse {
+  schema :: IntrospectionSchema 
+} deriving (Show, Eq, Generic, FromJSON)
+
+data IntrospectionSchema = IntrospectionSchema {
+    queryType :: RootTypeName
+  , mutationType :: Maybe RootTypeName
+  , subscriptionType :: Maybe RootTypeName
+  , types :: [OutputType]
+  , directives :: [Directive]
+}  deriving (Show, Eq, Generic, FromJSON)
+
+data RootTypeName = RootTypeName { name :: Text } deriving (Show, Eq, Generic, FromJSON)
+
+data OutputType = OutputType {
+      kind :: Text
+    , name :: Text
+    , description :: Maybe Text
+    , fields :: Maybe [Field]
+    , inputFields :: Maybe [TypeRef]
+    , interfaces ::  Maybe [TypeRef]
+    , enumValues :: Maybe [EnumValues]
+    , possibleTypes :: Maybe [TypeRef]
+} deriving (Show, Eq, Generic, FromJSON)
+
+
+data EnumValues = EnumValues {
+    name :: Text
+  , description :: Maybe Text
+  , isDeprecated :: Bool
+} deriving (Show, Eq, Generic, FromJSON)
+
+data Field = Field {
+      name :: Text
+    , description :: Maybe Text
+    , args :: [InputType]
+    , isDeprecated :: Bool
+    , deprecationReason :: Maybe Text
+    , typeRef :: TypeRef
+} deriving (Show, Eq, Generic, FromJSON)
+
+data TypeRef = TypeRef {
+    kind :: Text
+  , name :: Maybe Text
+  , ofType :: TypeRef
+} deriving (Show, Eq, Generic, FromJSON)
+
+data InputType = InputType {
+   name :: Text
+  , description :: Maybe Text
+  , typeRef :: TypeRef
+  , defaultValue :: Maybe Text
+} deriving (Show, Eq, Generic, FromJSON)
+
+data Directive = Directive {
+    name :: Text
+  , description :: Maybe Text
+  , locations :: [Text]
+  , args :: Maybe [InputType]
+} deriving (Show, Eq, Generic, FromJSON)
+
+
