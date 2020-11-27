791631662e6de1d94dcc0f69d61e193732baaa7f
certainty <david.krentzlin@gmail.com>
2020-11-27 08:14:04 +0100

    Use correct environment variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 set -euo pipefail
 
-alias demo="/root/.local/bin/graphqshell-exe --api http://demo_server:$GQL_SERVER_PORT/graphql"
+alias demo="/root/.local/bin/graphqshell-exe --api $GRAPHQL_API"
 echo "Just execute 'demo' to run the demo"
 
