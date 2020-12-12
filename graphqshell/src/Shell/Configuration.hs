4baf3ff518f4436868a999ab66ce528f38d6c693
certainty <david.krentzlin@gmail.com>
2020-12-12 12:57:41 +0100

    Add more validations

--- graphqshell/src/Shell/Configuration.hs
+++ graphqshell/src/Shell/Configuration.hs
@@ -6,14 +6,21 @@ import Text.URI (URI,mkURI)
 import Lens.Micro.Platform (makeLenses, (^.))
 import Control.Exception.Safe (MonadThrow, throw)
 import Data.Yaml (decodeThrow, withObject, withArray)
-import Data.Yaml.Aeson (FromJSON(..), (.:),(.:?), Parser, Value)
+import Data.Yaml.Aeson (FromJSON(..), (.:),(.:?),(.!=), Parser, Value)
 import Data.Vector hiding (sequence)
 import Validation
+import Data.List ((\\), nub)
 import qualified Data.List.NonEmpty as NonEmpty
 
 newtype ConfigurationError = ConfigurationError [ConfigurationErrorDetail] deriving (Eq, Show)
 
-data ConfigurationErrorDetail = MultipleDefaultEndpoints [Text] deriving (Eq, Show)
+data ConfigurationErrorDetail =
+    MultipleDefaultEndpoints [Text] 
+  | MultipleDefaultThemes [Text]
+  | DuplicateNames [Text]
+  | MissingDefaultEndpoint
+  | MissingDefaultTheme
+  deriving (Eq, Show)
 
 instance Exception ConfigurationError
 
@@ -41,7 +48,6 @@ data ThemeConfig = ThemeConfig {
   _themePath :: FilePath 
 } deriving (Generic, Show, Eq)
 
-
 makeLenses ''ApplicationConfig
 makeLenses ''EndpointConfig
 makeLenses ''EndpointHttpConfig
@@ -55,13 +61,41 @@ parseConfiguration inp = do
     (Success validatedCfg) -> pure validatedCfg
 
 validateConfiguration ::  ApplicationConfig -> Validation (NonEmpty ConfigurationErrorDetail) ApplicationConfig
-validateConfiguration = validateAll [validateSingleDefaultEndpoint]
+validateConfiguration = validateAll [
+  validateSingleDefaultEndpoint,
+  validateSingleDefaultTheme,
+  validateUniqueEndpointNames,
+  validateUniqueThemeNames
+  ]
   where
     validateSingleDefaultEndpoint cfg = do
       case Relude.map _endpointName (Relude.filter _endpointIsDefault (cfg ^. appConfigEndpoints)) of
+        [ ]       -> failure MissingDefaultEndpoint
         [_]       -> Success cfg
-        endpoints -> failure (MultipleDefaultEndpoints endpoints)   
-      
+        endpoints -> failure (MultipleDefaultEndpoints endpoints)
+        
+    validateSingleDefaultTheme cfg = do
+      case Relude.map _themeName (Relude.filter _themeIsDefault (cfg ^. appConfigThemes)) of
+        [ ]    -> failure MissingDefaultTheme
+        [_]    -> Success cfg
+        themes -> failure (MultipleDefaultThemes themes)
+
+    validateUniqueEndpointNames cfg = do
+      let duplicates = findDuplicates $ Relude.map _endpointName (cfg ^. appConfigEndpoints)
+      if Relude.null duplicates then
+        Success cfg
+      else failure (DuplicateNames duplicates)
+
+    validateUniqueThemeNames cfg = do
+      let duplicates = findDuplicates $ Relude.map _themeName (cfg ^. appConfigThemes)
+      if Relude.null duplicates then
+        Success cfg
+      else failure (DuplicateNames duplicates)
+
+
+    findDuplicates :: (Eq a) => [a] -> [a]
+    findDuplicates ls = ls \\ (nub ls) 
+
 
 instance FromJSON ApplicationConfig where
   parseJSON = withObject "ApplicationConfig" $ \o -> do
@@ -73,7 +107,7 @@ instance FromJSON EndpointConfig where
   parseJSON = withObject "EndpointConfig" $ \o -> do
     EndpointConfig
       <$> o .: "name"
-      <*> o .: "default"
+      <*> o .:? "default" .!= False 
       <*> ((o .: "url") >>= parseURI)
       <*> ((o .:? "link") >>= parseMaybeURI)
       <*> o .:? "http"
@@ -101,6 +135,6 @@ instance FromJSON ThemeConfig where
   parseJSON = withObject "ThemeConfig" $ \o -> do
     ThemeConfig
       <$> o .: "name"
-      <*> o .: "default"
+      <*> o .:? "default" .!= False
       <*> o .: "path"
     
--- graphqshell/test/Shell/ConfigurationTest.hs
+++ graphqshell/test/Shell/ConfigurationTest.hs
@@ -31,6 +31,58 @@ spec_parseConfiguration = do
     it "fails if two endpoints are marked as default" $ do
       parseConfiguration twoDefaultEndpoints `shouldThrow` (== ConfigurationError [MultipleDefaultEndpoints ["weather", "other"]])
       
+    it "fails if two endpoints have the same name" $ do
+      parseConfiguration duplicateEndpoints `shouldThrow` (== ConfigurationError [DuplicateNames ["weather"]])
+
+  describe "themes" $ do
+    it "fails if two themes are marked as default" $ do
+      parseConfiguration twoDefaultThemes `shouldThrow` (== ConfigurationError [MultipleDefaultThemes ["default", "other"]])
+      
+    it "fails if two themes have the same name" $ do
+      parseConfiguration duplicateThemes `shouldThrow` (== ConfigurationError [DuplicateNames ["weather"]])
+
+duplicateThemes :: ByteString      
+duplicateThemes = [r|
+endpoints:
+  - name: some
+    default: true
+    url: "https://example.com/api"
+themes:
+  - name: weather
+    default: true
+    path: /path/to/theme-file
+  - name: weather
+    path: /path/to/theme-file-foo
+|]
+
+duplicateEndpoints :: ByteString      
+duplicateEndpoints = [r|
+endpoints:
+  - name: weather
+    default: true
+    url: "https://example.com/api"
+  - name: weather
+    url: "https://example.com/api"
+themes:
+  - name: default
+    default: true
+    path: /path/to/theme-file
+|]     
+
+twoDefaultThemes :: ByteString      
+twoDefaultThemes = [r|
+endpoints:
+  - name: weather
+    default: true
+    url: "https://example.com/api"
+themes:
+  - name: default
+    default: true
+    path: /path/to/theme-file
+  - name: other
+    default: true
+    path: /path/to/theme-file
+|]     
       
 twoDefaultEndpoints :: ByteString      
 twoDefaultEndpoints = [r|
