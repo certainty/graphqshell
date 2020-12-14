86160babcb3e3f01ea68520dfdd6b014e8776756
David Krentzlin <certainty@users.noreply.github.com>
2020-12-14 17:03:29 +0100

    Use XdgConfig directory by default instead of home (#28)

--- graphqshell/src/Shell/Configuration.hs
+++ graphqshell/src/Shell/Configuration.hs
@@ -1,4 +1,3 @@
-
 module Shell.Configuration
   ( module Shell.Configuration.Types
   , Shell.Configuration.Validation.ValidationError(..)
@@ -7,22 +6,27 @@ module Shell.Configuration
   )
 where
 import           Relude
-import           Control.Exception.Safe         ( MonadThrow
-                                                , throw
-                                                )
+import           Control.Exception.Safe                                                 ( MonadThrow
+                                                                                        , throw
+                                                                                        )
 import           Shell.Configuration.Validation
 import           Shell.Configuration.Types
-import           Validation                     ( Validation(Success)
-                                                , Validation(Failure)
-                                                )
+import           Validation                                                             ( Validation
+                                                                                          ( Success
+                                                                                          )
+                                                                                        , Validation
+                                                                                          ( Failure
+                                                                                          )
+                                                                                        )
 import qualified Data.List.NonEmpty            as NonEmpty
-import           Data.Yaml                      ( decodeThrow )
+import           Data.Yaml                                                              ( decodeThrow
+                                                                                        )
 
-data  ConfigurationError = InvalidConfig [ValidationError] deriving (Eq, Show)
+data ConfigurationError = InvalidConfig [ValidationError]
+  deriving (Eq, Show)
 instance Exception ConfigurationError
 
-parseConfiguration
-  :: (MonadThrow m) => FilePath -> ByteString -> m ApplicationConfig
+parseConfiguration :: (MonadThrow m) => FilePath -> ByteString -> m ApplicationConfig
 parseConfiguration configFilePath fileContent = do
   cfg <- decodeThrow fileContent
   case fromMarshalled configFilePath cfg of
