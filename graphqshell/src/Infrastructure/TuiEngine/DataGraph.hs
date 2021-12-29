6a1fcc0c8839e949f11d312f380507198c0009d2
certainty <david.krentzlin@gmail.com>
2021-12-29 15:57:02 +0100

    Make keyword polymorphic

--- graphqshell/src/Infrastructure/TuiEngine/DataGraph.hs
+++ graphqshell/src/Infrastructure/TuiEngine/DataGraph.hs
@@ -1,84 +1,76 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Infrastructure.TuiEngine.DataGraph where
 
 import qualified Data.HashMap.Strict as Map
-import Infrastructure.TuiEngine.Keys (Key (Key))
 import Relude
-import Relude (Hashable)
 
---
--- { :foo/bar [:foo/baz :foo/qux] }
---
--- Let's star to shape the API from the usage side
---
--- properties
--- query #_ ["foo/baz"]
+newtype Keyword a = KV {unKeyword :: a}
+  deriving (Eq, Show, Ord, Hashable)
 
--- query ident
--- `query [("foo/bar" 123)]
+class ToKeyword a b where
+  toKeyword :: a -> Keyword b
 
-type Keyword = Text
-
-data Transaction = Ls [Transaction] | Prop Keyword | Join Transaction Transaction | Ident Keyword Transaction | Id Int deriving (Eq, Show)
+data Transaction kv = Ls [Transaction kv] | Prop (Keyword kv) | Join (Transaction kv) (Transaction kv) | Ident (Keyword kv) (Transaction kv) | Id Int deriving (Eq, Show)
 
 -- empty
 
-empty :: Transaction
+empty :: Transaction Text
 empty = Ls []
 
 -- properties
-properties :: Transaction
-properties = Ls [Prop "album/name", Prop "album/year"]
+properties :: Transaction Text
+properties = Ls [Prop (KV "album/name"), Prop (KV "album/year")]
 
 -- joins
-joins :: Transaction
+joins :: Transaction Text
 joins =
   Ls
     [ Join
-        (Prop "favorite-albums")
-        (Ls [Prop "album/name", Prop "album/year"])
+        (Prop (KV "favorite-albums"))
+        (Ls [Prop (KV "album/name"), Prop (KV "album/year")])
     ]
 
 -- nested joins
-nestedJoins :: Transaction
+nestedJoins :: Transaction Text
 nestedJoins =
   Ls
     [ Join
-        (Prop "favorite-albums")
+        (Prop (KV "favorite-albums"))
         ( Ls
-            [ Prop "album/name",
-              Prop "album/year",
+            [ Prop (KV "album/name"),
+              Prop (KV "album/year"),
               Join
-                (Prop "album-tracks")
-                (Ls [Prop "track/name", Prop "track/duration"])
+                (Prop (KV "album-tracks"))
+                (Ls [Prop (KV "track/name"), Prop (KV "track/duration")])
             ]
         )
     ]
 
 -- idents
-idents :: Transaction
-idents = Ident "album/name" (Id 1)
+idents :: Transaction Text
+idents = Ident (KV "album/name") (Id 1)
 
-data Property value = Value value | Pointer Keyword TableId deriving (Eq, Show)
+data Property kv value = Value value | Pointer (Keyword kv) TableId deriving (Eq, Show)
 
-data Entity value = Entity
+data Entity kv value = Entity
   { _entityId :: Int,
-    _entityProperties :: Map.HashMap Keyword (Property value)
+    _entityProperties :: Map.HashMap (Keyword kv) (Property kv value)
   }
   deriving (Eq, Show)
 
 newtype TableId = TableId Int deriving (Eq, Show, Hashable)
 
-data Table value = Table
-  { _tableName :: Keyword,
-    _rows :: Map.HashMap TableId (Entity value)
+data Table kv value = Table
+  { _tableName :: Keyword kv,
+    _rows :: Map.HashMap TableId (Entity kv value)
   }
 
-data Database value = Database
-  { _tables :: Map.HashMap Keyword (Table value)
+data Database kv value = Database
+  { _tables :: Map.HashMap (Keyword kv) (Table kv value)
   }
 
 --
@@ -87,46 +79,54 @@ data Database value = Database
 --  :ADDRESS { 42   {:address/id 42 :address/street "111 Main St."}}}
 --
 
-personTable :: Table Text
+personTable :: Table Text Text
 personTable =
   Table
-    "person"
+    (KV "person")
     ( Map.fromList
         [ ( TableId 1,
             Entity
               1
               ( Map.fromList
-                  [ ("person/id", Value "1"),
-                    ("person/name", Value "Joe"),
-                    ("person/address", Pointer "address" (TableId 42))
+                  [ (KV "person/id", Value "1"),
+                    (KV "person/name", Value "Joe"),
+                    (KV "person/address", Pointer (KV "address") (TableId 42))
                   ]
               )
           )
         ]
     )
 
-addressTable :: Table Text
+addressTable :: Table Text Text
 addressTable =
   Table
-    "address"
+    (KV "address")
     ( Map.fromList
         [ ( TableId 42,
             Entity
               42
-              (Map.fromList [("address/id", Value "1"), ("address/street", Value "111 Main St.")])
+              (Map.fromList [(KV "address/id", Value "1"), (KV "address/street", Value "111 Main St.")])
           )
         ]
     )
 
-database :: Database Text
+database :: Database Text Text
 database =
   Database
     ( Map.fromList
-        [ ( "person",
+        [ ( KV "person",
             personTable
           ),
-          ( "address",
+          ( KV "address",
             addressTable
           )
         ]
     )
+
+--- query
+
+-- query :: Transaction kv -> Database kv value -> [Entity kv value]
+-- query (Ls transactions) db = concatMap (queryTable transactions db) (Map.elems (db ^. tables))
+
+-- queryTable :: [Transaction kv] -> Database kv value -> a0 -> [Entity kv value]
+-- queryTable = error "not implemented"
