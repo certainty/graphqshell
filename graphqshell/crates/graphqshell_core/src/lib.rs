5548bd66fdaf6ba90fe7e90dc74a30e75069d34f
certainty <david.krentzlin@gmail.com>
2022-02-07 08:51:59 +0100

    Add application crate

--- graphqshell/crates/graphqshell_core/src/lib.rs
+++ graphqshell/crates/graphqshell_core/src/lib.rs
@@ -1,7 +1,6 @@
 extern crate anyhow;
 pub mod domain;
 pub mod infra;
-pub mod utils;
 
 #[cfg(test)]
 #[macro_use]
--- graphqshell/src/application/mod.rs
+++
@@ -1,7 +0,0 @@
-/// The application layer (in onion architecture)
-///
-/// The user or any other program interacts with the application(s) in this module.
-/// The module contain things like user interfaces, APIs, and provides orchestration for the domain
-/// logic. It might contain services and logic likewise.
-pub(crate) mod configuration;
-pub mod gui_app;
